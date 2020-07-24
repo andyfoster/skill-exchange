a59442cb817f8f3804ca0988d53f676a68021c92
Andy Foster <andrewfosteris@gmail.com>
2020-07-24 22:03:33 +1200

    Set up basic routes in Express

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res, next) => {
   res.send('Welcome to Express');
 });
+
+// Define Routes
+app.use('/api/v1/users', require('./routes/users'));
+app.use('/api/v1/auth', require('./routes/auth'));
+app.use('/api/v1/profile', require('./routes/profile'));
+app.use('/api/v1/posts', require('./routes/posts'));
 
 const PORT = process.env.PORT || 5000;
 
