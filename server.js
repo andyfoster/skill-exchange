d86b415620763d6325cc800951abfcc0b036af61
Andy Foster <andrewfosteris@gmail.com>
2020-07-24 22:55:22 +1200

    Bring in validation for user route

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res, next) => {
   res.send('Welcome to Express');
 });
