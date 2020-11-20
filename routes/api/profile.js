2c05120e11eb811854bb82ba49b03bdaade55c44
Andy Foster <andrewfosteris@gmail.com>
2020-11-20 13:31:56 +1300

    Create new route for adding education to user's profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -236,4 +236,50 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
 
 });
 
+
+// @route   PUT api/v1/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put('/education', [auth, [
+  check('school', 'School is required').not().isEmpty(),
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description,
+  } = req.body;
+
+  const newExp = {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description,
+  };
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.education.unshift(newExp);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+})
+
 module.exports = router;
