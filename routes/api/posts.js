46159698513341f32fc0a770498b979fad27ce48
Andy Foster <andrewfosteris@gmail.com>
2020-11-26 02:05:41 +1300

    Add route to delete a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -197,5 +197,36 @@ router.post('/comment/:id', [auth, [
 });
 
 
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete a comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+    // const comment = post.comments.filter(comment => comment._id === req.params.comment_id);
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({msg: 'Comment does not exist'});
+    }
+
+    // Check user owns the comment
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({msg: 'User not authorized'});
+    }
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+    post.comments.splice(removeId, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
