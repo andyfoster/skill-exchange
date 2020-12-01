4c6e66c81e410643f8eb3121ad2411ca744f2b51
Andy Foster <andrewfosteris@gmail.com>
2020-12-01 14:37:47 +1300

    Create new reducers: USER_LOADED and AUTH_ERROR

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
 } from '../actions/types';
 
 const initialState = {
@@ -14,6 +16,13 @@ export default function auth(state = initialState, action) {
   const { type, payload } = action;
 
   switch(type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
@@ -23,11 +32,12 @@ export default function auth(state = initialState, action) {
         loading: false
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
         ...payload,
-        isAuthenticated: true,
+        isAuthenticated: false,
         loading: false
       };
     default:
