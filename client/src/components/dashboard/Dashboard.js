76ec34083eb326e75da5ea0f3cec33f2a0e1f5f9
Andy Foster <andrewfosteris@gmail.com>
2020-12-03 17:46:32 +1300

    Create a profile reducer to load when user logs into dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Dashboard = props => {
+// Redux
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
   return (
     <div>
       Dashboard
@@ -10,7 +18,14 @@ const Dashboard = props => {
 }
 
 Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-}
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile
+});
 
-export default Dashboard;
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
