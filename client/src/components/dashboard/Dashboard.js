182cfc8d18058a16b84f5901f4c48f88430ec7b4
Andy Foster <andrewfosteris@gmail.com>
2020-12-11 13:36:30 +1300

    Show dashboard and its link when logged in

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,20 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import Spinner from '../../components/layout/Spinner';
 
 // Redux
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({ 
+  getCurrentProfile, 
+  auth: { user }, 
+  profile: {profile, loading} }) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+  return loading && profile === null ? <Spinner /> : 
+    <Fragment>
+      <h1 className="large text-primary">
+        Dashboard
+      </h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome, { user && user.name }
+      </p>
+      { profile !== null ? 
+      <Fragment>You have a profile</Fragment> : 
+      <Fragment>No profile</Fragment> }
+      <p className=""></p>
+    </Fragment>
 }
 
 Dashboard.propTypes = {
