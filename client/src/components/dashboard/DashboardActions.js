2beb5d54b808fa4b9f7427702d42b6d9b4de44e0
Andy Foster <andrewfosteris@gmail.com>
2020-12-18 15:05:05 +1300

    Tidy up dashboard actions links

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -5,15 +5,15 @@ export const DashboardActions = () => {
   return (
     <div className="dash-buttons">
       <Link to="/edit-profile" className="btn btn-light">
-        <i className="fas fa-user-circle text-primary"></i>
+        <i className="fas fa-user-circle text-primary"></i>{' '}
           Edit Profile
       </Link>
       <Link to="/add-experience" className="btn btn-light">
-        <i className="fas fa-black-tie text-primary"></i>
+        <i className="fab fa-black-tie text-primary"></i>{' '}
           Add Experience
       </Link>
       <Link to="/add-education" className="btn btn-light">
-        <i className="fas fa-graduation-cap text-primary"></i>
+        <i className="fas fa-graduation-cap text-primary"></i>{' '}
           Add Education
       </Link>
     </div>
