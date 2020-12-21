042e5d348bab4d86b9150e575183f09c1e5c3e63
Andy Foster <andrewfosteris@gmail.com>
2020-12-21 17:11:40 +1300

    Delete all user's posts when they delete their account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 const User = require('../../models/User');
 const { check, body, validationResult } = require('express-validator');
 const config = require('config');
@@ -156,6 +157,7 @@ router.get('/user/:user_id', async (req, res) => {
 router.delete('/', auth, async (req, res) => {
   try {
     // @TODO  - remove user's posts
+    await Post.deleteMany({ use: req.user.id });
 
     // Remote profile
     await Profile.findOneAndRemove({user: req.user.id});
